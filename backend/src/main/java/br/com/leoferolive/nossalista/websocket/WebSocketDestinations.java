6e7b6ac59d2e9f946b73ecfbbc6f5f0e88c25134
Leonardo <leoferolive@gmail.com>
2026-03-10 01:44:10 -0300

    feat(websocket): add user-level notification topic destination

--- backend/src/main/java/br/com/leoferolive/nossalista/websocket/WebSocketDestinations.java
+++ backend/src/main/java/br/com/leoferolive/nossalista/websocket/WebSocketDestinations.java
@@ -7,6 +7,7 @@ public final class WebSocketDestinations {
     public static final String LIST_TOPIC_PREFIX = "/topic/list/";
     public static final String ITEMS_SUFFIX = "/items";
     public static final String PRESENCE_SUFFIX = "/presence";
+    public static final String USER_TOPIC_PREFIX = "/topic/user/";
 
     private WebSocketDestinations() {
     }
@@ -17,5 +18,9 @@ public final class WebSocketDestinations {
 
     public static String listPresence(UUID listId) {
         return LIST_TOPIC_PREFIX + listId + PRESENCE_SUFFIX;
+    }
+
+    public static String userNotifications(UUID userId) {
+        return USER_TOPIC_PREFIX + userId + "/notifications";
     }
 }
